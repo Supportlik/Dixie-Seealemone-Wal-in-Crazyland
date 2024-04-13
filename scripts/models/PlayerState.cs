1327507385cdd9f8923d49a42369d3ddf785f823
Damian Kosellek <kosellek.damian@gmail.com>
2024-04-13 22:35:16 +0200

    update main

---
+++ scripts/models/PlayerState.cs
@@ -0,0 +1,15 @@
+﻿namespace MasterofElements.scripts.models;
+
+public enum PlayerState
+{
+    Death,
+    Fall,
+    Hurt,
+    Idle,
+    Jump,
+    SummonWater,
+    SummonFire,
+    SummonAir,
+    SummonEarth,
+    Walk,
+}
