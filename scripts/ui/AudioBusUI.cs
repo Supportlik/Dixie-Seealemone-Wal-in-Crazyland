1bdffe029426db2214b2ee3fc5c5da4ed6fadf5b
Pascal Fitzner <pascal-fitzner@web.de>
2024-04-14 12:32:13 +0200

    slider fix

--- scripts/ui/AudioBusUI.cs
+++ scripts/ui/AudioBusUI.cs
@@ -4,47 +4,50 @@ using Godot;
 using MasterofElements.scripts.singletons;
 
 public partial class AudioBusUI : VBoxContainer{
-
-    public Label audioLabel;
     private AutoLoader _autoLoader;
+
+    private Texture2D _masterLabelSprite = GD.Load<Texture2D>("res://assets/sprites/ui/Master_Labelx200.png");
+	private Texture2D _musicLabelSprite = GD.Load<Texture2D>("res://assets/sprites/ui/Music_Labelx200.png");
+	private Texture2D _sfxLabelSprite = GD.Load<Texture2D>("res://assets/sprites/ui/SFX_Labelx200.png");
     public override void _Ready()
     {
         _autoLoader = new AutoLoader(this);
-        Name = "AudioSettings";
-        audioLabel = new Label();
-        audioLabel.Name = Name +"Title";
-        audioLabel.Text = "Audio ";
-        AddChild(audioLabel);
         GenerateAudioBusUIElement();
     }
 
     private void GenerateAudioBusUIElement(){
-        AudioBusUIElement masterBusUIElement = new AudioBusUIElement("Master",_autoLoader.AudioService.GetMasterVolume());
+        AudioBusUIElement masterBusUIElement = new AudioBusUIElement("Master",_masterLabelSprite,_autoLoader.AudioService.GetMasterVolume());
         AddChild(masterBusUIElement);
-        AudioBusUIElement musicBusUIElement = new AudioBusUIElement("Music",_autoLoader.AudioService.GetMusicVolume());
+        AudioBusUIElement musicBusUIElement = new AudioBusUIElement("Music",_musicLabelSprite,_autoLoader.AudioService.GetMusicVolume());
         AddChild(musicBusUIElement);
-        AudioBusUIElement sfxBusUIElement = new AudioBusUIElement("SFX",_autoLoader.AudioService.GetMusicVolume());
+        AudioBusUIElement sfxBusUIElement = new AudioBusUIElement("SFX",_sfxLabelSprite,_autoLoader.AudioService.GetSFXVolume());
         AddChild(sfxBusUIElement);
     }
 
     public partial class AudioBusUIElement : VBoxContainer{
 
         private AutoLoader _autoLoader;
+
+        private string _name;
         private const int MAXVALUE = 1;
-        public Label BusLabel;
+        public TextureRect BusLabel;
         public HSlider BusSlider;
 
+        public VBoxContainer Container;
+
         private PackedScene _busSliderScene;
-        public AudioBusUIElement(string label, float initialDb){
-            _busSliderScene = GD.Load<PackedScene>("res://scenes/ui/sprite_slider.tscn");
-            BusLabel = new Label();
-            BusLabel.Text = label;
-            AddChild(BusLabel);
-            BusSlider = _busSliderScene.Instantiate<HSlider>();
-            BusSlider.MaxValue = MAXVALUE;
-            BusSlider.Value = (float)Math.Pow(10, initialDb / 20f);
-            BusSlider.Step = 0.01;
-            AddChild(BusSlider);
+        private PackedScene _conatainerScene;
+        public AudioBusUIElement(string name, Texture2D label, float sliderValue){
+            _name = name;
+            _conatainerScene = GD.Load<PackedScene>("res://scenes/ui/audio_ui_container.tscn");
+            Container = _conatainerScene.Instantiate<VBoxContainer>();
+            AddChild(Container);
+            InitSlider(sliderValue);
+            InitLabel(label);
+            Control control = new Control();
+            control.AddChild(BusLabel);
+            Container.AddChild(control);
+            Container.AddChild(BusSlider);
         }
 
         public override void _Ready()
@@ -53,21 +56,34 @@ public partial class AudioBusUI : VBoxContainer{
             BusSlider.ValueChanged += OnSliderChanged;
         }
 
+        public void InitSlider(float sliderValue){
+            _busSliderScene = GD.Load<PackedScene>("res://scenes/ui/sprite_slider.tscn");
+            BusSlider = _busSliderScene.Instantiate<HSlider>();
+            BusSlider.MaxValue = MAXVALUE;
+            BusSlider.MinValue = 0;
+            BusSlider.Step = 0.01;
+            BusSlider.Value = sliderValue;
+        }
+
+        private void InitLabel(Texture2D label){
+            BusLabel = new TextureRect();
+            BusLabel.Texture = label;
+        }
+        
+
         private void OnSliderChanged(double value)
         {
-            float decibelValue = (float)Math.Log10(value) * 20;
-            switch (BusLabel.Text)
+            switch (_name)
             {
                 case "Master":
                 GD.Print(value);
                 _autoLoader.AudioService.SetMasterVolume((float)value);
-                GD.Print(AudioServer.GetBusVolumeDb(0));
                 break;
                 case "Music":
-                _autoLoader.AudioService.SetMusicVolume(decibelValue);
+                _autoLoader.AudioService.SetMusicVolume((float)value);
                 break;
                 case "SFX":
-                _autoLoader.AudioService.SetSFXVolume(decibelValue);
+                _autoLoader.AudioService.SetSFXVolume((float)value);
                 break;
                 default:
                 break;
--- scripts/ui/StartMenu.cs
+++ scripts/ui/StartMenu.cs
@@ -11,6 +11,7 @@ public partial class StartMenu : Control
     public override void _Ready()
     {
         _autoLoader = new AutoLoader(this);
+        _autoLoader.AudioService.PlayMusic("Ludum_Dare_55_Menu_Music.mp3", this);
         startMenuController = GetNode<StartMenuController>("Control/ColorRect/Panel/GridContainer/Content/StartMenuContent");
         startMenuController.CloseStartMenu += HandleCloseStartMenu;
         settingsMenuController = GetNode<SettingsMenuController>("Control/ColorRect/Panel/GridContainer/Content/SettingsMenuContent");
